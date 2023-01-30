b99517ead6319499ddee06454334a143ce5fedf8
Ivan-F-Dev <89821080113@mail.ru>
2023-01-30 13:25:16 +0400

    add gh repo to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const Index = () => {
     return (
         <MainContainer title='Главная страница'>
             <h1 className={s.title}>Главная страница</h1>
-            <a className={s.git} href="https://github.com/Ivan-F-Dev">Github.com</a>
+            <a className={s.git} href="https://github.com/Ivan-F-Dev/azalia">Github.com</a>
             <a className={s.git} href="https://disk.yandex.ru/i/XtGVv8HPAewR2w">resume</a>
         </MainContainer>
     );
